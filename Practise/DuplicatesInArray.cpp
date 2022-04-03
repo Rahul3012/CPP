4a7cbb0b6a32d1370c0c648879e61284090e8cd0
Rahul3012 <rahullokhande3012@gmail.com>
2022-04-03 09:16:04 +0530

    Duplicate elements from array

---
+++ Practise/DuplicatesInArray.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 9;
+    // cin>>N;
+    int arr[N] = {1, 1, 2, 3, 2, 4, 5, 5, 4};
+    // for(int i=0;i<N;i++)
+    // {
+    //     cin>>a[i];
+    // }
+    int counter = 0;
+    int duplicate[N];
+    for (int j = 0; j < N - 1; j++)
+    {
+        for (int k = j + 1; k < N; k++)
+        {
+            if (arr[j] == arr[k])
+            {
+                int flag = 0;
+                for (int h = 0; h < counter; h++)
+                {
+                    if (duplicate[h] == arr[k])
+                    {
+                        flag = 1;
+                    }
+                }
+                if (flag == 0)
+                {
+                    duplicate[counter++] = arr[k];
+                }
+                flag = 0;
+            }
+        }
+    }
+
+    if (counter == 0)
+    {
+        cout << -1;
+    }
+    for (int g = 0; g < counter; g++)
+    {
+        cout << duplicate[g] << " ";
+    }
+    return 0;
+}
