cd01de6f202e1f517b698eb8c8678cb99c54c0ec
Rahul3012 <rahullokhande3012@gmail.com>
2022-06-14 10:14:25 +0530

    Stack implementation

---
+++ Data Structure/Stack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<int> stack1;
+    stack1.push(21);
+    stack1.push(22);
+    stack1.push(23);
+    stack1.push(24);
+    stack1.push(25);
+
+    while (!stack1.empty())
+    {
+        cout << ' ' << stack1.top();
+        stack1.pop();
+    }
+}
